13bca5b021f26f73230ab474182b3363a54c500f
JohnIsDimz <johnisdimz@gmail.com>
2026-07-27 01:07:11 +0700

    Delete KidsFamily directory

--- KidsFamily/app/src/main/java/com/xixfamily/kids/utils/Config.kt
+++
@@ -1,7 +0,0 @@
-package com.xixfamily.kids.utils
-
-object Config {
-    // Default server URL - change this to your server address
-    // Format: http://YOUR_SERVER_IP:3000
-    const val SERVER_URL = "http://192.168.1.1:3000"
-}
